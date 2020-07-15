0a94ec7ec58671fcc9b87d6133c423ab84902167
Alex Eisenhauer <eisea904@newschool.edu>
2020-07-15 19:01:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,10 +2,11 @@ require 'pry'
 
 def using_push(array, string)
   array.push(string)
+  array 
 end
 
 def using_unshift(array, string)
-  #binding.pry 
+  #binding.pry
   array.unshift(string)
   array
 end
